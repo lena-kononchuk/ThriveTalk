888c59e25377a5c40caa2a51f9eb7786a21d1a03
Lena Kononchuk <81321087+HakunaMatata91@users.noreply.github.com>
2022-02-09 15:00:33 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let modal = document.getElementById('popup');
+let btn = document.getElementById("modal");
+let span = document.querySelector(".close");
+
+btn.onclick = function (e) {
+   modal.style.visibility = "visible";
+   modal.style.opacity = "1";
+   e.preventDefault();
+}
+span.onclick = function (e) {
+   modal.style.visibility = "hidden";
+   modal.style.opacity = "0"
+   e.preventDefault();
+}
+
+window.onclick = function () {
+   if (e.target === modal) {
+      modal.style.visibility = "hidden";
+      modal.style.opacity = "0"
+   }
+}
